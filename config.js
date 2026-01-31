8b329691b117afbac971f0a7da956111522047d3
Majd Aljamed <fatimah.aljamed.7@gmail.com>
2026-01-31 04:31:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine for February 14th, 2025? ₊˚⊹♡ ", // The big question!
+            text: "Will you be my Valentine for February 14th, 2026? ₊˚⊹♡ ", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
